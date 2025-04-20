5b75f233eccff004626f97d7b0a2fb83585afc2b
alexisjbaptiste <78384349+alexisjbaptiste@users.noreply.github.com>
2025-04-20 17:55:14 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="onelinerml",
-    version="0.1.6",
+    version="0.1.8",
     description="A one-line machine learning library with API and dashboard deployment.",
     packages=find_packages(),
     install_requires=[
